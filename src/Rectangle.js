132dced1212835389fb40a0752c0b97774b78abd
Sim Tze Jan <tzejan@gmail.com>
2018-02-28 17:12:05 +0800

    Added Rectangle test for length

--- src/Rectangle.js
+++ src/Rectangle.js
@@ -1,6 +1,7 @@
 class Rectangle{
     constructor(breadth, length){
         this.breadth = breadth;
+        this.length = length;
     }
 };
 
--- tests/Rectangle.test.js
+++ tests/Rectangle.test.js
@@ -1,9 +1,14 @@
 var Rectangle = require('../src/Rectangle')
 
 describe('Unit tests for Rectangle class', function() {
-    it('should take in a breadth and length', function() {
+    it('should take in a breadth and length, and record the breadth', function() {
         var rect = new Rectangle(5, 10);
         expect(rect.breadth).toEqual(5);
     });
     
+    it('should take in a breadth and length, and record the length', function() {
+        var rect = new Rectangle(5, 10);
+        expect(rect.length).toEqual(10);
+    });
+
 });
