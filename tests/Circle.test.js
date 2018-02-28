18e9f98b54d50cd90db71c0318ad4ab122353216
Sim Tze Jan <tzejan@gmail.com>
2018-02-28 22:13:09 +0800

    Refactored Rectangle and Circle tests

--- tests/Circle.test.js
+++ tests/Circle.test.js
@@ -1,39 +1,44 @@
 var Circle = require("../src/Circle")
 
 describe('Unit tests for Circle class', function() {
-    it('should have a radius', function() {
-        var c = new Circle(1);
-        expect(c.radius).toEqual(1);
+    describe('Constructor tests', function() {
+        it('should have a radius', function() {
+            var c = new Circle(1);
+            expect(c.radius).toEqual(1);
+        });
     });
 
-    it('should have a area of 3.1 for a radius of 1', function() {
-        var c = new Circle(1);
-        expect(c.area()).toEqual(3.1);
+    describe('Area tests', function() {
+        it('should have a area of 3.1 for a radius of 1', function() {
+            var c = new Circle(1);
+            expect(c.area()).toEqual(3.1);
+        });
+
+        it('should have a area of 12.6 for a radius of 2', function() {
+            var c = new Circle(2);
+            expect(c.area()).toEqual(12.6);
+        });
+
+        it('should have a area of 28.3 for a radius of 3', function() {
+            var c = new Circle(3);
+            expect(c.area()).toEqual(28.3);
+        });
     });
 
-    it('should have a area of 12.6 for a radius of 2', function() {
-        var c = new Circle(2);
-        expect(c.area()).toEqual(12.6);
-    });
+    describe('Perimeter tests', function() {
+        it('should have a perimeter of 6.3 for a radius of 1', function() {
+            var c = new Circle(1);
+            expect(c.perimeter()).toEqual(6.3);
+        });
 
-    it('should have a area of 28.3 for a radius of 3', function() {
-        var c = new Circle(3);
-        expect(c.area()).toEqual(28.3);
-    });
+        it('should have a perimeter of 12.6 for a radius of 2', function() {
+            var c = new Circle(2);
+            expect(c.perimeter()).toEqual(12.6);
+        });
 
-    it('should have a perimeter of 6.3 for a radius of 1', function() {
-        var c = new Circle(1);
-        expect(c.perimeter()).toEqual(6.3);
+        it('should have a perimeter of 18.8 for a radius of 3', function() {
+            var c = new Circle(3);
+            expect(c.perimeter()).toEqual(18.8);
+        });
     });
-
-    it('should have a perimeter of 12.6 for a radius of 2', function() {
-        var c = new Circle(2);
-        expect(c.perimeter()).toEqual(12.6);
-    });
-
-    it('should have a perimeter of 18.8 for a radius of 3', function() {
-        var c = new Circle(3);
-        expect(c.perimeter()).toEqual(18.8);
-    });
-    
 });
--- tests/Rectangle.test.js
+++ tests/Rectangle.test.js
@@ -1,36 +1,38 @@
 var Rectangle = require('../src/Rectangle')
 
 describe('Unit tests for Rectangle class', function() {
-    it('should take in a breadth and length, and record the breadth', function() {
-        var rect = new Rectangle(5, 10);
-        expect(rect.breadth).toEqual(5);
-    });
-    
-    it('should take in a breadth and length, and record the length', function() {
-        var rect = new Rectangle(5, 10);
-        expect(rect.length).toEqual(10);
-    });
+    describe('Constructor tests', function() {
+        it('should take in a breadth and length, and record the breadth', function() {
+            var rect = new Rectangle(5, 10);
+            expect(rect.breadth).toEqual(5);
+        });
 
-    it('should return the area of a Rectangle of sides 2x4 as 8', function() {
-        var rect = new Rectangle(2, 4);
-        expect(rect.area()).toEqual(8);
+        it('should take in a breadth and length, and record the length', function() {
+            var rect = new Rectangle(5, 10);
+            expect(rect.length).toEqual(10);
+        });
     });
 
-    it('should return the perimeter of a Rectangle of sides 2x4 as 12', function() {
-        var rect = new Rectangle(2, 4);
-        expect(rect.perimeter()).toEqual(12);
+    describe('Area tests', function() {
+        it('should return the area of a Rectangle of sides 2x4 as 8', function() {
+            var rect = new Rectangle(2, 4);
+            expect(rect.area()).toEqual(8);
+        });
+
+        it('should return the area of a Rectangle of sides 5x10 as 50', function() {
+            var rect = new Rectangle(5, 10);
+            expect(rect.area()).toEqual(50);
+        });
     });
 
-     it('should return the area of a Rectangle of sides 5x10 as 50', function() {
-        var rect = new Rectangle(5, 10);
-        expect(rect.area()).toEqual(50);
+    describe('Perimeter tests', function() {
+        it('should return the perimeter of a Rectangle of sides 2x4 as 12', function() {
+            var rect = new Rectangle(2, 4);
+            expect(rect.perimeter()).toEqual(12);
+        });
+        it('should return the perimeter of a Rectangle of sides 5x10 as 30', function() {
+            var rect = new Rectangle(5, 10);
+            expect(rect.perimeter()).toEqual(30);
+        });
     });
-
-    it('should return the perimeter of a Rectangle of sides 5x10 as 30', function() {
-        var rect = new Rectangle(5, 10);
-        expect(rect.perimeter()).toEqual(30);
-    });
-
-
-
 });
