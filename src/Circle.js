640c9fa665e95ffe4ae14ce8277c3458fca39449
Sim Tze Jan <tzejan@gmail.com>
2018-02-28 17:34:08 +0800

    Added Circle tests for area

--- src/Circle.js
+++ src/Circle.js
@@ -2,6 +2,11 @@ class Circle{
     constructor(radius){
         this.radius = radius;
     }
+
+    area(){
+        var calcArea = Math.PI * this.radius ** 2;
+        return Math.round(calcArea * 10) / 10;
+    }
 }
 
 module.exports = Circle;
--- tests/Circle.test.js
+++ tests/Circle.test.js
@@ -5,5 +5,20 @@ describe('Unit tests for Circle class', function() {
         var c = new Circle(1);
         expect(c.radius).toEqual(1);
     });
+
+    it('should have a area of 3.1 for a radius of 1', function() {
+        var c = new Circle(1);
+        expect(c.area()).toEqual(3.1);
+    });
+
+    it('should have a area of 12.6 for a radius of 2', function() {
+        var c = new Circle(2);
+        expect(c.area()).toEqual(12.6);
+    });
+
+    it('should have a area of 28.3 for a radius of 3', function() {
+        var c = new Circle(3);
+        expect(c.area()).toEqual(28.3);
+    });
     
 });
