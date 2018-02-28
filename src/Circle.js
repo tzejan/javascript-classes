6b78c29a921cf09a83e6ea36db67a173d0e7d991
Sim Tze Jan <tzejan@gmail.com>
2018-02-28 21:44:51 +0800

    Added Circle test for perimeter

--- src/Circle.js
+++ src/Circle.js
@@ -7,6 +7,11 @@ class Circle{
         var calcArea = Math.PI * this.radius ** 2;
         return Math.round(calcArea * 10) / 10;
     }
+
+    perimeter(){
+        var calcPeri = 2 * Math.PI * this.radius;
+        return Math.round(calcPeri * 10) / 10
+    }
 }
 
 module.exports = Circle;
--- tests/Circle.test.js
+++ tests/Circle.test.js
@@ -20,5 +20,20 @@ describe('Unit tests for Circle class', function() {
         var c = new Circle(3);
         expect(c.area()).toEqual(28.3);
     });
+
+    it('should have a perimeter of 6.3 for a radius of 1', function() {
+        var c = new Circle(1);
+        expect(c.perimeter()).toEqual(6.3);
+    });
+
+    it('should have a perimeter of 12.6 for a radius of 2', function() {
+        var c = new Circle(2);
+        expect(c.perimeter()).toEqual(12.6);
+    });
+
+    it('should have a perimeter of 18.8 for a radius of 3', function() {
+        var c = new Circle(3);
+        expect(c.perimeter()).toEqual(18.8);
+    });
     
 });
